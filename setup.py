21fdfbc54e1ae6e7f4fe420141fb587953fefc2a
Rob685 <rtejada@stanford.edu>
2020-06-26 07:43:17 -0700

    Adding cmake to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exo_predict',
-    version= '1.0.27',
+    version='1.0.27',
     description='exo_predict will help you determine how likely you are to find an exoplanet!',
     url='https://github.com/Rob685/codeastro_ml_planets',
     author='Jea Adams, Rob Tejada, Sofia Covarrubias',
@@ -14,7 +14,8 @@ setup(
         'pandas',
         'sklearn',
         'numpy',
-        'xgboost==1.1.1',
-        'matplotlib'
+        'xgboost==1.2.0',
+        'matplotlib',
+        'cmake'
     ]
 )
