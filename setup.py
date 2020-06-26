a4209386d4db18512833382ca6e678620e6db70e
jeaadams <jadams21@amherst.edu>
2020-06-26 00:08:27 -0700

    Added version to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='orbitize',
-    version=get_property('__version__', 'codeastro_ml_planets'),
+    version= '1.0.0',
     description='exo_predict will help you determine how likely you are to find an exoplanet!',
     url='https://github.com/Rob685/codeastro_ml_planets',
     author='Jea Adams, Rob Tejada, Sofia Covarrubias',
