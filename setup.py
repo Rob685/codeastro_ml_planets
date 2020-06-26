a608e5f4999c982de8797a3f37927da4f54ca6fa
jeaadams <jadams21@amherst.edu>
2020-06-26 00:10:59 -0700

    Fixed typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages, Extension
 
 setup(
-    name='orbitize',
+    name='exo_predict',
     version= '1.0.0',
     description='exo_predict will help you determine how likely you are to find an exoplanet!',
     url='https://github.com/Rob685/codeastro_ml_planets',
