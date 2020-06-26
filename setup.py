9a20a26f2fcc42aacc9bddf7efba140351ead395
jeaadams <jadams21@amherst.edu>
2020-06-26 00:27:37 -0700

    Fixed bug in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='exo_predict',
-    version= '1.0.0',
+    version= '1.0.1',
     description='exo_predict will help you determine how likely you are to find an exoplanet!',
     url='https://github.com/Rob685/codeastro_ml_planets',
     author='Jea Adams, Rob Tejada, Sofia Covarrubias',
