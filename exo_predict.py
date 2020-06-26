9b3aa35963b32ce23b97e9fdfe530899b3aeca36
jeaadams <jadams21@amherst.edu>
2020-06-25 23:27:47 -0700

    Added new pickle file

--- exo_predict.py
+++ exo_predict.py
@@ -3,15 +3,16 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 import pickle as pkl
 
-
 class Predictor:
     """
     Class that predicts whether a system has more than one planet given a dataset that contains the following features: stellar mass, stellar temperature, stellar radius, and semi major axis of one planet
-
+    
     Args:
         df (pd.ataframe): dataframe containing all relevant columns
         mass_col (str): name of column containing stellar mass
@@ -56,7 +57,7 @@ class Predictor:
         return df
 
 # Load the model 
-model = pkl.load(open("num_planets_model.p", "rb"))
+model = pkl.load(open("data/num_planets_model.p", "rb"))
 
 def create_num_planets_predictor():
     """
